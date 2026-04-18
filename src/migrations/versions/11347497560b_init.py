cf6ed06247f1373225cd57437c54db95ca196fd3
Vladislav <vladislav.dr2@mail.ru>
2026-04-18 21:09:00 +0300

    chore: create init migration

---
+++ src/migrations/versions/11347497560b_init.py
@@ -0,0 +1,39 @@
+"""init
+
+Revision ID: 11347497560b
+Revises:
+Create Date: 2026-04-18 20:41:07.807685
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = "11347497560b"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute(
+        """
+    CREATE TABLE users (
+        id UUID PRIMARY KEY,
+        email VARCHAR UNIQUE NOT NULL,
+        password VARCHAR NOT NULL,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        father_name VARCHAR
+    );
+    """
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("DROP TABLE users;")
