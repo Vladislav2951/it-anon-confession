dfee13c78125e9cf28c515575de8e4541978de9e
Vladislav <vladislav.dr2@mail.ru>
2026-04-23 04:41:29 +0300

    style: remove useless code

--- src/core/security.py
+++ src/core/security.py
@@ -1,36 +1,6 @@
-from datetime import datetime, timedelta, timezone
 import hashlib
-import logging
-from typing import Any, Literal, Optional
 
 import bcrypt
-import jwt
-from pydantic import BaseModel
-
-from core.config import get_settings
-
-
-logger = logging.getLogger(__name__)
-
-settings = get_settings()
-
-ALGORITHM = "HS256"
-
-
-class TokenInfo(BaseModel):
-    access_token: str
-    token_type: Literal["Bearer"]
-
-
-def create_access_token(payload: dict[str, Any]) -> str:
-    to_encode = payload.copy()
-    now = datetime.now(timezone.utc)
-    expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    to_encode.update({"exp": expire, "iat": now})
-    return jwt.encode(
-        to_encode, settings.JWT_SECRET.get_secret_value(), algorithm=ALGORITHM
-    )
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -43,21 +13,3 @@ def get_password_hash(password: str) -> str:
 
 def get_token_hash(token: str) -> str:
     return hashlib.sha256(token.encode()).hexdigest()
-
-
-def decode_jwt(token: str) -> dict[str, Any]:
-    return jwt.decode(
-        token, settings.JWT_SECRET.get_secret_value(), algorithms=[ALGORITHM]
-    )
-
-
-# def validate_jwt(token: str) -> bool:
-#     try:
-#         # Check if the token has expired
-#         if decoded_token.get("exp", 0) > datetime.now(timezone.utc).timestamp():
-#             return decoded_token
-#         return None
-#     except jwt.PyJWTError as e:
-#         logger.debug("Decode JWT error: %s", e)
-#         return None  # Catches ExpiredSignatureError, InvalidTokenError etc.
-#     return False
